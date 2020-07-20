46a08a614878d013215bba0feac15bbba3a795c2
K Duc <kngovand@gmail.com>
2020-07-20 13:20:08 -0600

    calculator.php done

---
+++ calculator.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Calculator</title>
+</head>
+<body>
+
+<div>
+    <h1>Calculator</h1>
+</div>
+
+<p>
+    <form method="get" action="calculator.php">
+        <input name="value1">
+        <select name="action">
+            <option value="plus">+</option>
+            <option value="minus">+</option>
+        </select>
+        <input name="value2">
+        <input type="submit" name="submit" value="Submit">
+    </form>
+</p>
+
+<p>
+    <?php
+        if(isset($_GET["submit"])) {
+            $v1 = $_GET["value1"];
+            $v2 = $_GET["value2"];
+
+            if($_GET["action"] == "plus") {
+                $result = $v1 + $v2;
+            }
+
+            else if($_GET["action"] == "minus") {
+                $result = $v1 + $v2;
+            }
+        }
+    ?>
+</p>
+
+</body>
+</html>
