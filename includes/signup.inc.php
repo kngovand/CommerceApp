8b1b885f275b9a8aab4a5eb18001b6b8b5eef9dc
K Duc <kngovand@gmail.com>
2020-07-27 15:39:28 -0600

    signup.inc.php complete with placeholder/hashing sec

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,5 +1,6 @@
 <?php
 
+# if signup form is submitted
 if (isset($_POST['signup-submit'])) {
 
     require 'dbhandler.inc.php';
@@ -9,48 +10,90 @@ if (isset($_POST['signup-submit'])) {
     $pwd = $_POST['password1'];
     $pwdrepeat = $_POST['password2'];
 
-    // if any values are empty
+    # if any values are empty
     if(empty($username) || empty($email) || empty($pwd) || empty($pwdrepeat) ) {
         header("Location: ../signup.php?error=emptyfields&user=".$username."&email=".$email);
         exit();
     }
 
-    // legal email and username check
+    # legal email and username check
     else if(!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $username)) {
         header("Location: ../signup.php?error=invalidemailuid");
         exit();
     }
 
-    // only email check
+    # only email check
     else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         header("Location: ../signup.php?error=invalidemail&email=".$email);
         exit();
     }
 
-    // only username check
+    # only username check
     else if(!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
         header("Location: ../signup.php?error=invaliduser=".$username);
         exit();
     }
 
-    // passwords match 
+    # passwords match 
     else if($pwd !== $pwdrepeat) {
         header("Location: ../signup.php?error=checkpassword&user=".$username);
         exit();
     }
 
     else {
-        // prepared statement to prevent injection
+        # prepared statement to prevent injection
         $sql = "SELECT uidUsers from users where uidUsers=?";
         $stmt = mysqli_stmt_init($conn);
 
         if (!mysqli_stmt_prepare($stmt, $sql)) {
-            header("Location: ../signup.php?error=dberror");
+            header("Location: ../signup.php?error=sqlerror");
             exit();
         }
+        else {
+            # statement, placeholder, data
+            mysqli_stmt_bind_param($stmt, "s", $username);
+            mysqli_stmt_execute($stmt);
 
-        
+            # fetch
+            mysqli_stmt_store_result($stmt);
+
+            $resultCheck = mysqli_stmt_num_rows($stmt);
+
+            # resultCheck returns 1 entry exists
+            if ($resultCheck > 0) {
+                header("Location: ../signup.php?error=usertaken&mail=".$email);
+                exit();
+            }
+            else {
+                $sql = "INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES (?, ?, ?)";
+                $stmt = mysqli_stmt_init($conn);
+
+                if (!mysqli_stmt_prepare($stmt, $sql)) {
+                    header("Location: ../signup.php?error=sqlerror");
+                    exit();
+                }
+                else {
+                    # hashing password before insert statement executed
+                    $hashPwd = password_hash($pwd, PASSWORD_DEFAULT);
+
+                    mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashPwd);
+                    mysqli_stmt_execute($stmt);
+
+                    header("Location: ../signup.php?signup=success");
+                    exit();
+                }
+            }
+        }
+
+
     }
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+}
+else {
+    # redirect
+    header("Location: ../signup.php");
+    exit();
 }
 
 ?>
